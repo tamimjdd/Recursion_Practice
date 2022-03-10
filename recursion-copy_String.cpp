9536eed591c1f13c9a8193a180596b9c7b59bdf0
tamim wasif <40170742+tamimjdd@users.noreply.github.com>
2022-03-10 10:38:23 +0600

    Add files via upload

---
+++ recursion-copy_String.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX 10
+char s1[20],s2[20];
+void rec(string s1){
+    static int i=0;
+    if(s1[i]=='\0') {s2[i]=s1[i];return;}
+    else{
+        s2[i]=s1[i];
+        i++;
+        rec(s1);
+    }
+}
+
+
+// Driver Code
+int main()
+{
+
+    cin>>s1;
+
+    rec(s1);
+    cout<<s2<<endl;
+}
+
+// This is code is contributed by rathbhupendra
+
+
