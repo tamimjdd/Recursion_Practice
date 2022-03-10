baabce941ded1ac313c8a871ba90981a280d1eba
tamim wasif <40170742+tamimjdd@users.noreply.github.com>
2022-03-10 10:52:41 +0600

    Add files via upload

---
+++ recursion-first_capital_string.cpp
@@ -0,0 +1,30 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void rec(char s[],int sizes){
+    static int i=0;
+    if(i==sizes) {return;}
+    else{
+        if(int(s[i])>=65 && int(s[i])<=90){
+            cout<<s[i];
+            return;
+        }
+        i++;
+        rec(s,sizes);
+    }
+}
+
+
+// Driver Code
+int main()
+{
+
+
+    char s[]="tamimwasifTamimwasif";
+    int sizes=sizeof(s);
+    rec(s,sizes);
+}
+
+
+
